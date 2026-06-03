af6bfce3449190b9d7025398f9dd3d1049c10ee3
Srikkant Srinivasan <srikkant@live.com>
2026-06-03 08:34:46 +0530

    chore: added .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((odin-mode . ((eval . (with-eval-after-load 'dape
+                         (add-to-list 'dape-configs
+                                      '(odin-codelldb
+                                        modes (odin-mode)
+                                        command "codelldb"
+                                        command-args ("--port" :autoport)
+                                        port :autoport
+                                        :type "lldb"
+                                        :request "launch"
+                                        :program "nbody"
+                                        :cwd ".")))))))
