1b91ac7972a3bd6c6e0b9ee91da74ebf651d79ad
Srikkant Srinivasan <srikkant@live.com>
2026-06-11 07:31:09 +0530

    chore: updated local emacs config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,15 @@
-((odin-mode . ((eval . (with-eval-after-load 'dape
-                         (add-to-list 'dape-configs
-                                      '(odin-codelldb
-                                        modes (odin-ts-mode)
-                                        command "codelldb"
-                                        command-args ("--port" :autoport)
-                                        port :autoport
-                                        compile (lambda () (concat "make -C " (project-root (project-current)) " debug"))
-                                        :type "lldb"
-                                        :request "launch"
-                                        :program (lambda () (concat (project-root (project-current)) "nbody"))
-                                        :cwd (lambda () (project-root (project-current))))))))))
+((odin-ts-mode . ((eval . (with-eval-after-load 'dape
+                            (add-to-list 'dape-configs
+                                         '(odin-codelldb
+                                           modes (odin-ts-mode)
+                                           command "codelldb"
+                                           command-args ("--port" :autoport)
+                                           port :autoport
+                                           compile (lambda () (concat "make -C " (project-root (project-current)) " debug"))
+                                           :type "lldb"
+                                           :request "launch"
+                                           :program (lambda () (concat (project-root (project-current)) "nbody"))
+                                           :cwd (lambda () (project-root (project-current)))))))
+                  (eval . (after! apheleia
+                            (setf (alist-get 'odin-fmt apheleia-formatters) '("odinfmt" "-stdin"))
+                            (setq-local apheleia-formatter 'odin-fmt))))))))
